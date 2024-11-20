69fdd04b61274d60cc31455a51156f406e575d8f
MultiD822 <130605573+MultiD822@users.noreply.github.com>
2024-11-20 10:58:23 -0800

    This is a test comment

---
+++ BST_Test/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int a = 8;
+        int b = 10;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
